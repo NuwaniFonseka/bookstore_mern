60dd2d2f91b64d1351731d79105af8f2242dd66c
NuwaniFonseka <nuwaniprasansa@gmail.com>
2024-07-07 23:41:29 +0530

    create update delete

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 3001;
 
-export const mongoDBURL = 'mongodb+srv://root:root@cluster0.pbkva8v.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+export const mongoDBURL = 'mongodb+srv://root:root@cluster0.pbkva8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.get('/', (request,response) => {
     return response.status(243).send('WELCOME TO MERN STACK');
 });
 
+//save a new book
 app.post('/books', async (request, response) => {
     try{
         if(
@@ -33,6 +34,81 @@ app.post('/books', async (request, response) => {
         return response.status(201).send(book);
 
     }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
+//get all books from database
+app.get('/books', async (request, response) =>{
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
+//get one book from database
+app.get('/books/:id', async (request, response) =>{
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    }catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//update book from database
+app.put('/books/:id', async (request, response) =>{
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear 
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields'
+            });
+        }
+
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id,request.body);
+
+        if(!result){
+            return response.status(404).send({message: 'Book not found'});
+        }
+        return response.status(200).send({message: 'Book updated successfully'});
+
+    }catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
+//delete book from database
+
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).send({message: 'Book not found'});
+        }
+        return response.status(200).send({message: 'Book deleted successfully'});
+        
+    } catch (error) {
         console.log(error.message);
         response.status(500).send({message: error.message});
     }
